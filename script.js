9a9ce989a19e305f37ac86980795f347639ff77f
amanda-dc <68808193+amanda-dc@users.noreply.github.com>
2020-08-25 19:16:44 +0800

    basic game framework

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 // Disable ESLint rule no-unused-vars on next line because this code
 // is a learning tool and not a full-fledged node app.
-// eslint-disable-next-line no-unused-vars
+
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var rpcGameOutput = rockPaperScissors();
+  console.log (rpcGameOutput)
+
+  if (input == rpcGameOutput ) {
+    var myOutputValue = 'draw';
+  }
+  
+  else if ((input == 'paper' && rpcGameOutput == 'scissors') || (input == 'rock' && rpcGameOutput == 'paper')|| (input == 'scissors' && rpcGameOutput == 'rock')) {
+   var myOutputValue = 'lose';
+  }
+  else if   ((input == 'scissors' && rpcGameOutput == 'paper') || (input == 'paper' && rpcGameOutput == 'rock')|| (input == 'rock' && rpcGameOutput == 'scissors'))  {
+    var myOutputValue = 'win';
+  }
+
   return myOutputValue;
+  
 };
+
+
+
+
+var rockPaperScissors = function () {
+  var randomDecimal = Math.random() * 3;
+  var randomInteger = Math.floor ( randomDecimal );
+  var rpcResult = randomInteger + 1;
+  
+
+  if (rpcResult == 1) {
+    var rpcGameOutput = 'rock';
+ }
+ else if (rpcResult == 2) {
+  var rpcGameOutput = 'paper';
+}
+else if (rpcResult == 3) {
+  var rpcGameOutput = 'scissors';
+}
+  return rpcGameOutput;
+}
