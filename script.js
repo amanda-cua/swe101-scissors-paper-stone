08ca83fc7a8339b55782603e0e0ece61f49f5d7a
amanda-dc <68808193+amanda-dc@users.noreply.github.com>
2020-08-29 09:40:03 +0800

    progress on debugging

--- script.js
+++ script.js
@@ -1,48 +1,71 @@
 // Disable ESLint rule no-unused-vars on next line because this code
 // is a learning tool and not a full-fledged node app.
 
-
-var UserWinCounter = 0;
-var CompWinCounter = 0;
+var userWinCounter = 0;
+var compWinCounter = 0;
+var currentGameMode = "waiting for username";
+var userName = " ";
 
 var main = function (input) {
-  var rpcGameOutput = rockPaperScissors();
-  console.log(rpcGameOutput)
-  if (input == "rock" && rpcGameOutput == "scissors") {
-    var myOutputValue = 'Win'
-    UserWinCounter += 1
-      ;
-  } else if (input == "scissors" && rpcGameOutput == "paper") {
-    var myOutputValue = 'Win'
-    UserWinCounter += 1;
-  } else if (input == "paper" && rpcGameOutput == "rock") {
-    var myOutputValue = 'Win'
-    UserWinCounter += 1;
-  } else if (input == rpcGameOutput) {
-    var myOutputValue = 'Draw';
-  } else {
-    var myOutputValue = 'Lose'
-    CompWinCounter += 1;
+  
+  var myOutputValue = ' ';
+
+  if (currentGameMode == 'waiting for username') { 
+    userName = input;
+
+    if (input == "rock" && rpcGameOutput == "scissors") {
+      myOutputValue = 'Win'
+     userWinCounter += 1
+       ;
+    } else if (input == "scissors" && rpcGameOutput == "paper") {
+      myOutputValue = 'Win'
+     userWinCounter += 1;
+    } else if (input == "paper" && rpcGameOutput == "rock") {
+      myOutputValue = 'Win! '
+     userWinCounter += 1;
+    } else if (input == rpcGameOutput) {
+      myOutputValue = 'Draw! ';
+    } else  { input 
+      myOutputValue = 'Lose! '
+     compWinCounter += 1;
+    }  
+
+    currentGameMode = 'Rock Paper Scissors Game';
+
+     myOutputValue = "Hello "+ userName + "Type rock, paper, or scissors!";
+  } else   {
+     rpcGameOutput = rockPaperScissors()
   }
-  return myOutputValue + " Your score is " + UserWinCounter + " The copmuter score is " + CompWinCounter;
+  
+    return myOutputValue
+
 };
+
+
+ 
+ 
+ // myOutputValue + "<br>" + "Your score is " + userWinCounter +"." + "<br>" + "The computer's score is " + compWinCounter + "."}
+  
+
+
 
 
 // Computer randomly selecting what to throw
 
+
+
 var rockPaperScissors = function () {
   var randomInteger = Math.floor (Math.random() * 3);
   var rpcResult = randomInteger + 1;
-  
+  var rpcGameOutput;
 
   if (rpcResult == 1) {
-    var rpcGameOutput = 'rock';
- }
- else if (rpcResult == 2) {
+   rpcGameOutput = 'rock';
+  } else if (rpcResult == 2) {
   var rpcGameOutput = 'paper';
-}
-else if (rpcResult == 3) {
-  var rpcGameOutput = 'scissors';
-}
-  return rpcGameOutput;
-}
+  } else if (rpcResult == 3) {
+   rpcGameOutput = 'scissors';
+  } 
+return rpcGameOutput;
+};
+
